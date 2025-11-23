7a4d562abe4abbea2c4b43a8ba89bb45ecadb0f0
vraju123 <87394394+vraju123@users.noreply.github.com>
2025-11-23 14:15:54 -0800

    Finished Backend Implementation

---
+++ frontend/src/api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const API_BASE = "http://localhost:8000";
+
+export async function createTransaction(data) {
+  const res = await axios.post(`${API_BASE}/transactions`, data);
+  return res.data;
+}
+
+export async function fetchSummary(startDate, endDate) {
+  const res = await axios.get(`${API_BASE}/transactions`, {
+    params: { start_date: startDate, end_date: endDate },
+  });
+  return res.data;
+}
